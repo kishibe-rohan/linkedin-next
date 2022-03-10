025ba5c6c3c86edac7bf84bfb558a1978c6aea0e
kishibe-rohan <devrohan2000@gmail.com>
2022-03-10 20:31:53 +0530

    added api handlers for posts

---
+++ pages/api/auth/[...nextauth].js
@@ -0,0 +1,19 @@
+import NextAuth from "next-auth";
+import GoogleProvider from "next-auth/providers/google";
+import { MongoDBAdapter } from "@next-auth/mongodb-adapter";
+
+export default NextAuth({
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    }),
+  ],
+  secret: process.env.JWT_SECRET,
+  pages: {
+    signIn: "/home",
+  },
+  session: {
+    strategy: "jwt",
+  },
+});
